408cacc5ea3ccdb6f023a6498c1bfcacc2ce1e02
DashaKorotkevich <dashakorotkevich6878@gmail.com>
2025-06-16 15:55:55 +0300

    feat: компонент поста и пропсы

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
 
 function App() {
   const [value, setValue] = useState('ТЕКСТ В ИНПУТ')
 
   return (
     <div class='App'>
-      <Counter/>
-      <ClassCounter/>
+     <PostItem post={{id: 1, title: 'JS', body: 'Description'}}/>
+   
     </div>
   );
 }
